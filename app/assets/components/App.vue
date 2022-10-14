1cf79c4d1d47ddb68f6eb5305820b058a2ff115b
Gourab Sahu <grv_sh@yahoo.co.in>
2022-10-14 21:40:20 +0200

    Home page until unfinished "hurry-up div"

--- app/assets/components/App.vue
+++ app/assets/components/App.vue
@@ -1,20 +1,27 @@
 <script setup>
 
+import router from "../routes";
+
+function goToHome () {
+  router.push('/');
+}
 
 </script>
 
 <template>
   <div id="container">
     <div class="nav-bar">
-      <div class="logo">
-        Pastor Thomas
+      <div class="logo" @click="goToHome">
+        <img id="logo-image" src="https://www.thomasadeloach.com/wp-content/uploads/2020/11/DM-Logo-Header-v2.png">
       </div>
       <div id="nav-links">
-        <router-link to="/" class="nav-link">Home</router-link>
-        <router-link to="/about" class="nav-link">About</router-link>
-        <router-link to="/episodes" class="nav-link">Episodes</router-link>
-        <router-link to="/blog" class="nav-link">Blog</router-link>
-        <router-link to="/contact" class="nav-link">Contact</router-link>
+        <div class="nav-links-wrapper">
+          <router-link to="/" class="nav-link">Home</router-link>
+          <router-link to="/about" class="nav-link">About</router-link>
+          <router-link to="/episodes" class="nav-link">Episodes</router-link>
+          <router-link to="/blog" class="nav-link">Blog</router-link>
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+        </div>
       </div>
     </div>
 
@@ -27,37 +34,54 @@
 
 <style scoped>
 #container {
-  margin: auto;
-  width: 80%;
+  border: 2px solid red;
+}
+
+#logo-image {
+  width: 200px;
 }
 
 .nav-bar {
   display: flex;
   justify-content: space-between;
-  padding: 10px;
+  padding: 15px;
   border: 1px solid black;
 }
 
 #nav-links {
-  width: 400px;
+  width: 600px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-right: 40px;
+}
+.nav-links-wrapper {
   display: flex;
   justify-content: space-between;
+  height: 26px;
 }
 
 .nav-link {
   text-decoration: none;
   color: black;
   border-bottom: 1px solid transparent;
+  font-size: 22px;
 }
 
-.router-link-active, .nav-link:hover {
+.router-link-active {
   border-bottom: 1px solid black;
 }
 
+/*.nav-link:hover {
+  background: #727272;
+}*/
+
+.logo:hover {
+  cursor: pointer;
+}
+
 #view {
-  display: flex;
-  justify-content: center;
-  padding: 20px;
+
 }
 
 </style>
--- app/assets/components/Home.vue
+++ app/assets/components/Home.vue
@@ -1,3 +1,110 @@
+<script setup>
+
+
+</script>
+
 <template>
-  Home
+  <div class="home">
+    <div class="hero">
+
+    </div>
+    <div class="hero-sub">
+      <div class="pastor-thomas">
+        <img src="https://www.thomasadeloach.com/wp-content/uploads/2020/11/Pastor-Deloach-cool-stance-black-and-white-fade.png">
+      </div>
+      <div class="blank-left">
+
+      </div>
+      <div class="listen-to-podcasts">
+        <div class="podcast-wrapper">
+          <a href="https://thomasadeloach.podbean.com/" target="_blank">
+            <div class="podcast">
+              <b>Listen To My Podcast On Podbean</b>
+            </div>
+          </a>
+          <a href="https://podcasts.apple.com/us/podcast/2-empower-u-with-thomas-a-deloach/id1512656694" target="_blank">
+            <div class="podcast">
+              <b>Listen To My Podcast On Apple</b>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="about-me">
+      <h2 class="heading">About Me</h2>
+      <div class="content">
+        Pastor DeLoach was born in Louisville, Alabama and was later brought to Canton, Ohio by his mother.
+        He attended Canton Public Schools, where he was very active. At the age of 13, Pastor DeLoach taught himself how to play the drums. He likewise became quite good in playing two other instruments.
+        Grateful for the talents bestowed on him, he vowed to give all the glory and honor back to God – not only through music but in all significant aspects of his life.
+      </div>
+      <div class="read-more">
+        <router-link to="/about">Read More...</router-link>
+      </div>
+    </div>
+
+    <div class="empowerment">
+      <h2 class="heading">
+        "2 Empower U Podcast" Brings You:
+      </h2>
+
+      <div class="content">
+        <div class="empowerment-feature">
+          <div class="empower-icon">
+            <i class="fa-solid fa-podcast"></i>
+          </div>
+          <h4 class="empower-heading">Self-Empowerment </h4>
+          <div class="empower-text">Take control of your own life with God’s guidance and make positive choices. Stay confident & faithful in Him. </div>
+        </div>
+        <div class="empowerment-feature">
+          <div class="empower-icon">
+            <i class="fa-solid fa-circle-user"></i>
+          </div>
+          <h4 class="empower-heading">Building Partnership with God </h4>
+          <div class="empower-text">Acknowledge how things are impossible without God. Two is always better than one. </div>
+        </div>
+        <div class="empowerment-feature">
+          <div class="empower-icon">
+            <i class="fa-solid fa-bus"></i>
+          </div>
+          <h4 class="empower-heading">Strategy & Execution of Goals </h4>
+          <div class="empower-text">Set goals, make the right activities to reach your desired results. And don’t forget His spiritual guidance. Always. </div>
+        </div>
+        <div class="empowerment-feature">
+          <div class="empower-icon">
+            <i class="fa-solid fa-book"></i>
+          </div>
+          <h4 class="empower-heading">Mentorship </h4>
+          <div class="empower-text">Being a trainer is serious business but I believe I can coach, educate, equip, and advise you on how to live a spirit led life with these episodes as well as other sessions I can provide for you. </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="hurry-limited-slots">
+      <div class="announcements">
+        <div class="text">
+          <div class="paragraph"><span style="color: #EF4423">Hurry!</span> Limited slots only!</div>
+          <div class="paragraph">Subscribe and get your</div>
+          <div class="paragraph">FREE Material Now!</div>
+        </div>
+        <div class="buttons">
+          <div class="button">
+            SIGN UP NOW
+          </div>
+          <div class="button">
+            DONATE TO MY PODCAST
+          </div>
+        </div>
+      </div>
+      <div class="micro-phone">
+
+      </div>
+    </div>
+  </div>
+
 </template>
+
+<style>
+
+
+</style>
